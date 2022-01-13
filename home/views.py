becedc0e8aa4019b23fb30aa5db1f2de486b854a
asbenjamin <shammahbenjamin@gmail.com>
2022-01-13 17:00:21 +0300

    added some html and fixes

--- home/views.py
+++ home/views.py
@@ -7,4 +7,14 @@ def HomeView(request):
       
     # render function takes argument  - request
     # and return HTML as response
-    return render(request, "home/home.html")
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+
+        'num_visits': num_visits,
+    }
+
+    return render(request, "home/home.html", context=context)
