66b4887c34eab73bcf413d60410d8690fbd39052
asbenjamin <shammahbenjamin@gmail.com>
2022-01-14 23:38:46 +0300

    Edited settings

--- diary/settings.py
+++ diary/settings.py
@@ -146,3 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/home'
+
+#This logs any emails sent to the console (so you can copy the password reset link from the console).
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
